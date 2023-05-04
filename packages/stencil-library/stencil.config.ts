4dca7e09b229239b1a3090303e03ee093c5b07c4
Gabor Kiacz <info@lilagoz.hu>
2023-05-04 15:27:30 +0200

    built

--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -6,6 +6,7 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'dist',
+      esmLoaderPath: '../loader',
     },
     angularOutputTarget({
       componentCorePackage: 'stencil-library',
