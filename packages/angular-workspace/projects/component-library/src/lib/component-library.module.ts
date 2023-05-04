c3d716fbdeb109ae531284f23c7c6d2bd7ed08dc
Gabor Kiacz <info@lilagoz.hu>
2023-05-04 15:29:53 +0200

    component-library.module.ts

--- packages/angular-workspace/projects/component-library/src/lib/component-library.module.ts
+++ packages/angular-workspace/projects/component-library/src/lib/component-library.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { ComponentLibraryComponent } from './component-library.component';
-
-
+import { DIRECTIVES } from './stencil-generated';
 
 @NgModule({
   declarations: [
-    ComponentLibraryComponent
+    ...DIRECTIVES
   ],
   imports: [
   ],
   exports: [
-    ComponentLibraryComponent
+    ...DIRECTIVES
   ]
 })
 export class ComponentLibraryModule { }
