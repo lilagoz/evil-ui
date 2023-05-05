2d1753f132512bb261bc1c9707e128bd6bd78866
Gabor Kiacz <info@lilagoz.hu>
2023-05-05 10:34:05 +0200

    sass

--- packages/stencil-library/src/components/ez-input/ez-input.tsx
+++ packages/stencil-library/src/components/ez-input/ez-input.tsx
@@ -1,23 +1,20 @@
-import { Component, h } from '@stencil/core';
+import {Component, h, Host} from '@stencil/core';
 
 @Component({
   tag: 'ez-input',
-  styleUrl: 'ez-input.css',
+  styleUrl: 'ez-input.scss',
   shadow: true,
 })
 export class EzInput {
 
   render() {
     return (
-          <h1>This is the way! ⚔️</h1>
-    );
-/*    return (
       <Host>
         <slot>
-          <h1>This is the way! ⚔️</h1>
+          <h1>This is the way! ⚔️ 🛡️</h1>
         </slot>
       </Host>
-    );*/
+    );
   }
 
 }
