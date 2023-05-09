89592c2e45382bafd6d372a966757d15c170314a
Gabor Kiacz <info@lilagoz.hu>
2023-05-09 15:31:20 +0200

    menu

--- packages/angular-workspace/projects/my-app/src/app/app.component.ts
+++ packages/angular-workspace/projects/my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  constructor(private router: Router) {}
+  public get activeMenu() {
+    return this.router.url;
+  }
 }
