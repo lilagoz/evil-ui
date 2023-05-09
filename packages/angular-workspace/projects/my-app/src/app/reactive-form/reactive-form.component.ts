f5b6150b2517397b237bbb364263fb6f9c2955f5
Gabor Kiacz <info@lilagoz.hu>
2023-05-09 15:04:31 +0200

    reactive form

--- packages/angular-workspace/projects/my-app/src/app/reactive-form/reactive-form.component.ts
+++ packages/angular-workspace/projects/my-app/src/app/reactive-form/reactive-form.component.ts
@@ -5,9 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss'],
 })
-export class ReactiveFormComponent {
-  onSubmit(ev: any) {
-    ev.preventDefault();
-    console.log('I did something. 🐁');
-  }
-}
+export class ReactiveFormComponent {}
--- packages/angular-workspace/projects/my-app/src/app/template-driven-form/template-driven-form.component.ts
+++ packages/angular-workspace/projects/my-app/src/app/template-driven-form/template-driven-form.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.scss']
+  styleUrls: ['./template-driven-form.component.scss'],
 })
 export class TemplateDrivenFormComponent {
-
+  public phoneNumber: string = '🐁';
+  onSubmit(ev: any) {
+    ev.preventDefault();
+    const response = `I did something. ${this.phoneNumber}`;
+    console.log(response);
+    alert(response);
+  }
 }
