b22d4fedba3bca95603a7234ee73c7c14809e433
Gabor Kiacz <info@lilagoz.hu>
2023-05-12 14:00:54 +0200

    size

--- packages/stencil-library/src/components/sizeable-window/sizeable-window.tsx
+++ packages/stencil-library/src/components/sizeable-window/sizeable-window.tsx
@@ -15,13 +15,49 @@ export class SizeableWindow {
     height: number;
   } = { width: 640, height: 480, top: 200, left: 200 };
 
-  dragWindow: { screenX: number; screenY: number };
+  dragWindow: { screenX: number; screenY: number; cmd: string };
 
   log = (ev: MouseEvent) => console.log(ev.type, ev);
-  mouseHandler(ev: MouseEvent) {
+
+  moveWindow(ev: MouseEvent) {
+    const dX = ev.screenX - this.dragWindow.screenX;
+    const dY = ev.screenY - this.dragWindow.screenY;
+    this.dragWindow = { screenX: ev.screenX, screenY: ev.screenY, cmd: this.dragWindow.cmd };
+    const newParams = { ...this.windowParams };
+    switch (this.dragWindow.cmd) {
+      case 'move':
+        newParams.left += dX;
+        newParams.top += dY;
+        break;
+      case 'right':
+        newParams.width += dX;
+        break;
+      case 'left':
+        newParams.width -= dX;
+        newParams.left += dX;
+        break;
+      case 'bottom':
+        newParams.height += dY;
+        break;
+      case 'left-bottom':
+        newParams.height += dY;
+        newParams.width -= dX;
+        newParams.left += dX;
+        break;
+      case 'right-bottom':
+        newParams.height += dY;
+        newParams.width += dX;
+        break;
+    }
+
+    this.windowParams = {
+      ...newParams,
+    };
+  }
+  mouseHandler(ev: MouseEvent, cmd?: string) {
     switch (ev.type) {
       case 'mousedown':
-        this.dragWindow = { screenX: ev.screenX, screenY: ev.screenY };
+        this.dragWindow = { screenX: ev.screenX, screenY: ev.screenY, cmd };
         // this.log(ev);
         break;
       case 'mouseup':
@@ -30,15 +66,7 @@ export class SizeableWindow {
         break;
       case 'mousemove':
         if (this.dragWindow) {
-          const dX = ev.screenX - this.dragWindow.screenX;
-          const dY = ev.screenY - this.dragWindow.screenY;
-          this.dragWindow = { screenX: ev.screenX, screenY: ev.screenY };
-
-          this.windowParams = {
-            ...this.windowParams,
-            left: this.windowParams.left + dX,
-            top: this.windowParams.top + dY,
-          };
+          this.moveWindow(ev);
         }
 
         break;
@@ -60,7 +88,7 @@ export class SizeableWindow {
               height: `${this.windowParams.height}px`,
             }}
           >
-            <div class={'header'} onMouseDown={ev => this.mouseHandler(ev)}>
+            <div class={'window-header'} onMouseDown={ev => this.mouseHandler(ev, 'move')}>
               <div class={'header-button red-button'}></div>
               <div class={'header-button yellow-button'}></div>
               <div class={'header-button green-button'}></div>
@@ -68,8 +96,15 @@ export class SizeableWindow {
                 <div>{this.windowTitle}</div>
               </div>
             </div>
+            <div class={'window-content'}>
+              <slot></slot>
+            </div>
 
-            <slot></slot>
+            <div class={'window-sizer-left'} onMouseDown={ev => this.mouseHandler(ev, 'left')}></div>
+            <div class={'window-sizer-right'} onMouseDown={ev => this.mouseHandler(ev, 'right')}></div>
+            <div class={'window-sizer-bottom'} onMouseDown={ev => this.mouseHandler(ev, 'bottom')}></div>
+            <div class={'window-sizer-left-bottom'} onMouseDown={ev => this.mouseHandler(ev, 'left-bottom')}></div>
+            <div class={'window-sizer-right-bottom'} onMouseDown={ev => this.mouseHandler(ev, 'right-bottom')}></div>
           </div>
         </div>
       </Host>
