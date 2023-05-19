c26fc91211493c063a6746ad4c04d67c5854ed8c
Gabor Kiacz <info@lilagoz.hu>
2023-05-19 12:51:43 +0200

    vue

---
+++ packages/vue-my-app/src/components/evil-phonenumber/evil-phonenumber.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps({
+  foo: String,
+  modelValue: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const onValueChange = (ev: any) => {
+  console.log("e2", ev);
+  emit("update:modelValue", ev.target.value);
+};
+</script>
+
+<template>
+  <h6>evil-phonenumber {{ foo }}</h6>
+  <evil-phonenumber-input
+    @valueChange="onValueChange($event)"
+  ></evil-phonenumber-input>
+</template>
+
+<style scoped></style>
--- packages/vue-my-app/src/views/AboutView.vue
+++ packages/vue-my-app/src/views/AboutView.vue
@@ -1,15 +1,26 @@
-<script></script>
+<script lang="ts" setup>
+import evilPhonenumber from "../components/evil-phonenumber/evil-phonenumber.vue";
+import { ref } from "vue";
+const foo = ref("kukac");
+const bar = ref("");
+</script>
 
 <template>
-  <sizeable-window></sizeable-window>
+  <sizeable-window windowTitle="cim-cim">
+    <div class="form-item">
+      <p>cirmoscicahaj</p>
+      <evilPhonenumber v-model="bar" foo="prop-test"></evilPhonenumber>
+      <pre>value: {{ bar }}</pre>
+    </div>
+    <div class="form-item">
+      <input v-model="foo" />
+      <pre>{{ foo }}</pre>
+    </div>
+  </sizeable-window>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style lang="scss">
+.form-item {
+  margin: 1em;
 }
 </style>
